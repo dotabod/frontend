5573e3fef4bdd7ea880d4cd30d41ee9be9843f7f
Matt <Geczy@users.noreply.github.com>
2022-11-10 18:14:16 -0600

    fix schema!

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,7 +21,6 @@ export const authOptions: NextAuthOptions = {
   callbacks: {
     async session({ token, session }) {
       if (token) {
-        // @ts-ignore do we actually need ID ??
         session.user.id = token.id
         session.user.name = token.name
         session.user.email = token.email
