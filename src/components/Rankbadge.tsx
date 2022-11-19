f55db436635ca3d49e9c4982eff99b0b53392e5c
Matt <Geczy@users.noreply.github.com>
2022-11-18 21:00:05 -0600

    white text rank badge

--- src/components/Rankbadge.tsx
+++ src/components/Rankbadge.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export const Rankbadge = ({ image, leaderboard, rank }) => {
   return (
-    <div className="flex flex-col items-center rounded-md bg-slate-500/50 p-1 shadow-md">
+    <div className="flex flex-col items-center rounded border-t border-gray-400/50 bg-gray-500/50 p-1">
       <Image
         priority
         alt="rank badge"
@@ -11,7 +11,7 @@ export const Rankbadge = ({ image, leaderboard, rank }) => {
         height={56}
         src={`/images/ranks/${image}`}
       />
-      <span className="text-xs text-yellow-500">
+      <span className="text-xs text-white/60">
         {leaderboard && '#'}
         {rank}
       </span>
