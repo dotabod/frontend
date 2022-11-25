296a14eec9c724705627c7f9842ac6a5d0ff3c6b
Geczy <Geczy@users.noreply.github.com>
2022-11-25 09:15:59 -0600

    pull out the badge

--- src/components/Rankbadge.tsx
+++ src/components/Rankbadge.tsx
@@ -2,16 +2,23 @@
 import Image from 'next/image'
 import { Card } from './Card'
 
+export const Badge = ({ image, ...props }) => {
+  return (
+    <Image
+      priority
+      alt="rank badge"
+      width={56}
+      height={56}
+      src={`/images/ranks/${image}`}
+      {...props}
+    />
+  )
+}
+
 export const Rankbadge = ({ image, leaderboard, rank }) => {
   return (
     <Card>
-      <Image
-        priority
-        alt="rank badge"
-        width={56}
-        height={56}
-        src={`/images/ranks/${image}`}
-      />
+      <Badge image={image} />
       <span>
         {leaderboard && '#'}
         {rank}
