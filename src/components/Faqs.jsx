79e71bea57e2b2fedb8a0a843d70a8037c368a62
Matt <Geczy@users.noreply.github.com>
2022-12-01 08:16:23 -0600

    siwtch to discord link

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -85,10 +85,10 @@ export function Faqs() {
           <p className="mt-2 text-lg text-gray-600">
             If you have anything else you want to ask,{' '}
             <Link
-              href="mailto:info@dotabod.com"
+              href="https://discord.dotabod.com"
               className="text-gray-900 underline"
             >
-              reach out to us
+              reach out to us on discord
             </Link>
             .
           </p>
