8b572afb087629c72b8ed07286b0fb93cc45fbe8
Matt <Geczy@users.noreply.github.com>
2022-12-05 17:50:21 -0600

    show who's allowed to use what command

--- src/components/Dashboard/Features/CommandsCard.tsx
+++ src/components/Dashboard/Features/CommandsCard.tsx
@@ -1,7 +1,9 @@
 import { Card } from '@/ui/card'
-import { Toggle } from '@geist-ui/core'
+import { Badge, Toggle } from '@geist-ui/core'
 import { useUpdateSetting } from '@/lib/useUpdateSetting'
 import { CommandDetail } from '@/pages/dashboard/commands'
+
+const all = ['Moderators', 'Broadcaster', 'Everyone']
 
 export default function CommandsCard({
   command,
@@ -15,7 +17,29 @@ export default function CommandsCard({
       <div className="flex justify-between">
         <div>
           <div className="title">
-            <h3>{command.title}</h3>
+            <h3>
+              {command.title}{' '}
+              <div className="ml-4 inline space-x-2">
+                {' '}
+                {command.allowed === 'mods' && (
+                  <>
+                    <Badge type="success" className="!bg-green-800" scale={0.5}>
+                      Moderator
+                    </Badge>
+                    <Badge type="success" className="!bg-red-800" scale={0.5}>
+                      Broadcaster
+                    </Badge>
+                  </>
+                )}
+                {command.allowed === 'all' && (
+                  <>
+                    <Badge type="default" scale={0.5}>
+                      Everyone
+                    </Badge>
+                  </>
+                )}
+              </div>
+            </h3>
             <div className="subtitle">{command.description}</div>
           </div>
           {command.response && <command.response dark />}
