33ebd4369d3c476e523ba39ca17f6d977cb9c45f
Matt <Geczy@users.noreply.github.com>
2022-12-09 11:22:59 -0600

    add more troubleshoots

--- src/pages/dashboard/troubleshoot.tsx
+++ src/pages/dashboard/troubleshoot.tsx
@@ -9,6 +9,10 @@ const faqs = [
       'Check that you placed the cfg file in the correct folder. It goes in /gamestate_integration/, not in /cfg/. Add -gamestateintegration to Dota launch options. Restart Dota. OBS dotabod browser source must be above your other sources so it doesnt get blocked. Right click the dotabod source in preview, click transform, and click fit to content so it resizes and fills your screen.',
   },
   {
+    question: 'Dotabod keeps saying play a match, no steam id?',
+    answer: 'Add the OBS overlay, step 3 of setup.',
+  },
+  {
     question: 'MMR not tracking?',
     answer:
       'Enter your current MMR in the dashboard so that it isnt 0. You must also add the dotabod overlay to OBS for MMR tracking to start. It will not track without the overlay.',
@@ -17,6 +21,11 @@ const faqs = [
     question: 'How do I test that it works?',
     answer:
       'Try loading a solo bot match. If OBS is showing your overlays, it works. Also type !ping in your Twitch chat to make sure dotabod can type.',
+  },
+  {
+    question: 'Can I still use 9kmmrbot?',
+    answer:
+      "I'd remove it if I were you. Dotabod has all the features 9kmmrbot had, and more. Visit https://www.twitch.tv/popout/9kmmrbot/chat and type !part to remove 9kmmrbot.",
   },
   {
     question: "Why do bets open right when I pick? Can't I get counter picked?",
