9936ba0045e64b562fbc0170dd6f26aa4c37634f
Geczy <Geczy@users.noreply.github.com>
2022-12-23 07:50:12 -0500

    fix: sizing update to badges again

--- src/components/Rankbadge.tsx
+++ src/components/Rankbadge.tsx
@@ -13,7 +13,11 @@ export const Rankbadge = ({
     <Card {...props}>
       <Badge transformRes={transformRes} image={image} />
       <span
-        className={leaderboard ? '-mt-2' : '-mt-4'}
+        className={
+          leaderboard || ['80.png', '92.png'].includes(image)
+            ? '-mt-1'
+            : '-mt-3'
+        }
         style={{
           fontSize: transformRes ? transformRes({ height: 22 }) : 16,
         }}
