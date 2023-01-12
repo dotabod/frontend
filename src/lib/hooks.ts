4596f437ba54fef83a921a2a45d1e3edb713a7a3
Matt <Geczy@users.noreply.github.com>
2023-01-11 23:41:20 -0600

    lint: prettier

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,13 +1,13 @@
-"use client"
+'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export const useBaseUrl = (append?: string) => {
-  const [baseUrl, setBaseUrl] = useState("")
+  const [baseUrl, setBaseUrl] = useState('')
 
   useEffect(() => {
-    if (typeof window === "undefined") return
-    const base = window.location.protocol + "//" + window.location.host
+    if (typeof window === 'undefined') return
+    const base = window.location.protocol + '//' + window.location.host
     setBaseUrl(append ? `${base}/${append}` : base)
   }, [append])
 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth"
+import NextAuth from 'next-auth'
+import { authOptions } from '@/lib/auth'
 
 // @see ./lib/auth
 export default NextAuth(authOptions)
