733a6a92bafb8e8aa4d9e0ffd00813bbe5cca49a
Matt <Geczy@users.noreply.github.com>
2023-01-23 16:17:09 -0600

    fix commands being weird animation
    closes https://github.com/dotabod/frontend/issues/31

--- src/pages/dashboard/commands.tsx
+++ src/pages/dashboard/commands.tsx
@@ -148,7 +148,7 @@ export default function CommandsPage() {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
             <AnimatePresence>
               {filteredCommands.map((key) => (
-                <motion.div layout key={key}>
+                <motion.div key={key}>
                   <CommandsCard id={key} command={CommandDetail[key]} />
                 </motion.div>
               ))}
