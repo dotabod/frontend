00297bd5d146870565dedb8beb973f7b1baaae28
Matt <Geczy@users.noreply.github.com>
2023-01-23 19:24:56 -0600

    use sentry user

--- src/components/FullStorySession.tsx
+++ src/components/FullStorySession.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/nextjs'
 import * as FullStory from '@fullstory/browser'
 
 import { useEffect } from 'react'
@@ -18,6 +19,16 @@ const FullStorySession = () => {
     })
   }, [])
   useEffect(() => {
+    if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+      Sentry.setUser({
+        id: data?.user?.id,
+        username: data?.user?.name,
+        email: data?.user?.email,
+        twitchId: data?.user?.twitchId,
+        locale: data?.user?.locale,
+      })
+    }
+
     if (!process.env.NEXT_PUBLIC_FULLSTORY_ORG_ID) return
 
     if (status === 'authenticated') {
