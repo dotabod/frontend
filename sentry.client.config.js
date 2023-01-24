b1bbcceb61d9dd7dc9780333993986439e854f3c
Matt <Geczy@users.noreply.github.com>
2023-01-23 19:20:42 -0600

    use sentry replay session

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -11,8 +11,12 @@ if (SENTRY_DSN)
     dsn: SENTRY_DSN,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
+
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 1.0,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+    integrations: [new Sentry.Replay()],
   })
