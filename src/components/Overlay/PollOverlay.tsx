70213e9f0db6fd4b93ad1c873864def5bdadbe7c
Matt <Geczy@users.noreply.github.com>
2023-01-24 16:31:44 -0600

    ui: get ready for anim labels later

--- src/components/Overlay/PollOverlay.tsx
+++ src/components/Overlay/PollOverlay.tsx
@@ -1,11 +1,33 @@
 import TwitchFetcher from 'twitch-fetcher'
 import { motionProps } from '@/ui/utils'
 import { Center, Progress } from '@mantine/core'
-import { motion } from 'framer-motion'
+import { motion, useMotionValue, useSpring } from 'framer-motion'
 import Countdown, { zeroPad } from 'react-countdown'
 import { useTransformRes } from '@/lib/hooks/useTransformRes'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useGetSettings } from '@/lib/hooks/useUpdateSetting'
+
+function AnimatedNumber({ from, to }) {
+  const ref = useRef(null)
+  const motionValue = useMotionValue(from)
+  const springValue = useSpring(motionValue)
+
+  useEffect(() => {
+    motionValue.set(to)
+  }, [motionValue, to])
+
+  useEffect(
+    () =>
+      springValue.onChange((latest) => {
+        if (ref.current) {
+          ref.current.textContent = latest.toFixed(0)
+        }
+      }),
+    [springValue]
+  )
+
+  return <span ref={ref} />
+}
 
 export type PollData = {
   title: string
