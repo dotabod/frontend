47faa28f9834f132fb6f04cd895f22dbed9915ec
Matt <Geczy@users.noreply.github.com>
2023-01-23 19:41:32 -0600

    dont block things

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -18,5 +18,10 @@ if (SENTRY_DSN)
     // If the entire session is not sampled, use the below sample rate to sample
     // sessions when an error occurs.
     replaysOnErrorSampleRate: 1.0,
-    integrations: [new Sentry.Replay()],
+    integrations: [
+      new Sentry.Replay({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
   })
