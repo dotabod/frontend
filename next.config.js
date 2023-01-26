c1c5b5671440079abe36cf5bc281a197fb18f326
Matt <Geczy@users.noreply.github.com>
2023-01-26 17:51:03 -0600

    set release specifically
    https://github.com/getsentry/sentry-javascript/issues/5734#issuecomment-1261483723

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const moduleExports = {
   },
 
   sentry: {
+    release: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
     autoInstrumentMiddleware: !!(
       process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
     ),
