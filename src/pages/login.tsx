154bf12f4d73c409afc2d9d5363e7bbf2c70b084
Matt <Geczy@users.noreply.github.com>
2023-02-07 09:50:23 -0600

    fix: error message to show to invalid logins
    closes https://github.com/dotabod/frontend/issues/52

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,8 @@ import { UserAuthForm } from '@/components/Homepage/AuthForm'
 import { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { showNotification } from '@mantine/notifications'
+import Link from 'next/link'
 
 export default function Login() {
   const { status } = useSession()
@@ -15,6 +17,30 @@ export default function Login() {
       void router.push('/dashboard')
     }
   }, [router, status])
+
+  useEffect(() => {
+    if (router.asPath.toLowerCase().includes('error')) {
+      showNotification({
+        id: 'login-error',
+        title: 'Unable to log you in',
+        disallowClose: true,
+        autoClose: 50000,
+        message: (
+          <div>
+            <Link
+              href="https://discord.dotabod.com"
+              target="_blank"
+              className="text-blue-400 hover:text-blue-300"
+            >
+              Reach out to us on Discord
+            </Link>{' '}
+            to get help
+          </div>
+        ),
+        color: 'red',
+      })
+    }
+  }, [router.asPath])
 
   if (status === 'authenticated') return null
 
