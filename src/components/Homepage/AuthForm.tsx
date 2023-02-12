10c419afdb90605f463edb8ebec93e986220dcf6
Geczy <Geczy@users.noreply.github.com>
2023-02-12 17:07:04 -0600

    wip: antd conversion

--- src/components/Homepage/AuthForm.tsx
+++ src/components/Homepage/AuthForm.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { signIn } from 'next-auth/react'
 import clsx from 'clsx'
 import { useSearchParams } from 'next/navigation'
-import { LoadingOverlay } from '@mantine/core'
+import { Button } from 'antd'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -11,10 +11,11 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const searchParams = useSearchParams()
 
   return (
-    <div className={clsx('grid gap-6', className)} {...props}>
-      <button
-        type="button"
-        className="relative inline-flex w-full items-center justify-center space-x-2 rounded-lg border bg-white px-5 py-2.5 text-center text-sm font-medium text-black hover:bg-slate-100 focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 disabled:opacity-50 dark:hover:bg-[#050708]/30 dark:focus:ring-slate-500"
+    <div className={clsx(className)} {...props}>
+      <Button
+        loading={isLoading}
+        block
+        className="!border-slate-200 !bg-slate-100 !text-dark-500"
         onClick={() => {
           setIsLoading(true)
           signIn('twitch', {
@@ -30,38 +31,33 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
             })
         }}
         disabled={isLoading}
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 48 48"
+            width="48px"
+            height="48px"
+            className="h-4 w-4"
+            aria-hidden="true"
+            focusable="false"
+            data-prefix="fab"
+            data-icon="twitch"
+            role="img"
+          >
+            <path
+              fill="#FFF"
+              d="M12 32L12 8 39 8 39 26 33 32 24 32 18 38 18 32z"
+            />
+            <path
+              fill="#8E24AA"
+              d="M9,5l-3,7.123V38h9v5h5l5-5h7l10-10V5H9z M38,26l-5,5h-9l-5,5v-5h-6V9h25V26z"
+            />
+            <path fill="#8E24AA" d="M32 25h-5V15h5V25zM24 25h-5V15h5V25z" />
+          </svg>
+        }
       >
-        <LoadingOverlay
-          visible={isLoading}
-          loaderProps={{ size: 'lg', color: 'pink' }}
-          overlayOpacity={0.3}
-          overlayColor="#c5c5c5"
-          className="rounded"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-          width="48px"
-          height="48px"
-          className="mr-2 h-4 w-4"
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fab"
-          data-icon="twitch"
-          role="img"
-        >
-          <path
-            fill="#FFF"
-            d="M12 32L12 8 39 8 39 26 33 32 24 32 18 38 18 32z"
-          />
-          <path
-            fill="#8E24AA"
-            d="M9,5l-3,7.123V38h9v5h5l5-5h7l10-10V5H9z M38,26l-5,5h-9l-5,5v-5h-6V9h25V26z"
-          />
-          <path fill="#8E24AA" d="M32 25h-5V15h5V25zM24 25h-5V15h5V25z" />
-        </svg>
-        <span>Sign in with Twitch</span>
-      </button>
+        Sign in with Twitch
+      </Button>
     </div>
   )
 }
