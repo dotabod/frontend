e23afa40121fa70e976ad301b725e25c78ee2562
Matt <Geczy@users.noreply.github.com>
2023-02-18 10:48:45 -0600

    fix: use 7tv happi

--- src/components/Dashboard/ChatBot.tsx
+++ src/components/Dashboard/ChatBot.tsx
@@ -22,7 +22,7 @@ const emotesRequired = [
   },
   {
     label: 'Happi',
-    url: 'https://betterttv.com/emotes/634042bce6cf26500b42ce56',
+    url: 'https://7tv.app/emotes/63ef8f3532154feca8df8f0c',
   },
   { label: 'Madge', url: 'https://7tv.app/emotes/60a95f109d598ea72fad13bd' },
   { label: 'POGGIES', url: 'https://7tv.app/emotes/60af1b5a35c50a77926314ad' },
