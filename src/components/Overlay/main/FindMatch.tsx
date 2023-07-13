a449ba1629592c1fa97e2e297469efc457e5218d
Matt Gates <Geczy@users.noreply.github.com>
2023-07-13 11:34:36 +0200

    new: show the old match timer instead of a queue image blocker

--- src/components/Overlay/main/FindMatch.tsx
+++ src/components/Overlay/main/FindMatch.tsx
@@ -21,36 +21,7 @@ export const FindMatch = () => {
         Main menu
       </span>
 
-      <motion.div
-        key="queue-blocker-class"
-        {...motionProps}
-        style={{
-          bottom: res({ h: 0 }), // correct is n
-          right: res({ w: 0 }), // correct is 50
-        }}
-        className="absolute"
-      >
-        <div
-          style={{
-            width: res({ w: 330 }),
-            height: res({ h: 49 }),
-            right: res({ w: 59 }),
-            bottom: res({ h: 22 }),
-          }}
-          className="absolute flex items-center justify-center overflow-hidden"
-        >
-          <span
-            style={{
-              fontSize: res({ w: 26 }),
-            }}
-            className="font-outline-2 whitespace-nowrap font-bold uppercase tracking-[0.15em] text-white"
-          >
-            play dota
-          </span>
-        </div>
-
-        <MatchTimer res={res} />
-      </motion.div>
+      <MatchTimer res={res} />
     </>
   )
 }
