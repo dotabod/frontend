dcddf1a706461fbc81c79fc268cf71e6a3f01982
Matt Gates <Geczy@users.noreply.github.com>
2024-03-22 08:43:46 -0500

    wip

--- src/lib/api-middlewares/with-methods.ts
+++ src/lib/api-middlewares/with-methods.ts
@@ -1,7 +1,7 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next'
 
 export function withMethods(methods: string[], handler: NextApiHandler) {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
     if (!methods.includes(req.method)) {
       return res.status(405).end()
     }
--- src/pages/api/featured-users.ts
+++ src/pages/api/featured-users.ts
@@ -3,8 +3,50 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/lib/db'
 import { withMethods } from '@/lib/api-middlewares/with-methods'
 
+const usersWithErrors = []
+
+async function validateAccessToken(accessToken: string) {
+  const url = 'https://id.twitch.tv/oauth2/validate'
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+  }
+
+  try {
+    const response = await fetch(url, { headers })
+
+    if (response.ok) {
+      const data = await response.json()
+      return data
+    }
+  } catch (error) {
+    console.error('Failed to validate access token:', error)
+  }
+
+  return null
+}
+
+// Helper function to refresh access token
+async function refreshAccessToken(refreshToken: string) {
+  const url = `https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}`
+
+  try {
+    const response = await fetch(url, { method: 'POST' })
+    if (!response.ok) {
+      throw new Error(`Failed to refresh access token: ${response.statusText}`)
+    }
+    const data = await response.json()
+    return data.access_token
+  } catch (error) {
+    return null
+  }
+}
+
 // Helper function to fetch follower count for a user
-async function fetchFollowerCount(providerAccountId, accessToken) {
+async function fetchFollowerCount(
+  providerAccountId,
+  accessToken,
+  refreshToken
+) {
   const url = `https://api.twitch.tv/helix/channels/followers?broadcaster_id=${Number(
     providerAccountId
   )}`
@@ -15,7 +57,21 @@ async function fetchFollowerCount(providerAccountId, accessToken) {
 
   try {
     const response = await fetch(url, { headers })
+    if (response.status === 401) {
+      // Refresh access token
+      const newAccessToken = await refreshAccessToken(refreshToken)
+      if (newAccessToken) {
+        // Retry with new access token
+        return fetchFollowerCount(
+          providerAccountId,
+          newAccessToken,
+          refreshToken
+        )
+      }
+    }
     if (!response.ok) {
+      // log the streamed awaited response body
+      // console.log(await response.text())
       throw new Error(
         `Failed to fetch followers for providerAccountId ${providerAccountId}: ${response.statusText}`
       )
@@ -28,51 +84,167 @@ async function fetchFollowerCount(providerAccountId, accessToken) {
 }
 
 // Main function to update followers count
-async function updateFollows() {
+async function updateFollows(res: NextApiResponse) {
+  if (process.env.NODE_ENV !== 'development') {
+    console.log('Not in development mode, skipping updateFollows')
+    return
+  }
+
+  const pageSize = 100
+  let page = 0
+  const updates = []
+
   try {
+    console.log('fetching new users', page)
     const users = await prisma.user.findMany({
-      select: {
-        id: true,
-        name: true,
-        Account: {
-          select: {
-            providerAccountId: true,
-            access_token: true,
-          },
-        },
-      },
-      orderBy: {
-        createdAt: 'desc',
-      },
+      skip: page * pageSize,
+      take: pageSize,
       where: {
+        Account: {
+          requires_refresh: false,
+        },
         followers: null,
+      },
+      include: {
+        Account: true,
       },
     })
 
-    for (const user of users) {
-      if (!user.Account?.providerAccountId) continue
+    console.log(users.length)
 
-      const totalFollowerCount = await fetchFollowerCount(
+    const promises = users.map((user) => {
+      if (!user.Account?.providerAccountId) return null
+
+      // Return a promise that resolves to an object containing the user and the follower count
+      return fetchFollowerCount(
         user.Account.providerAccountId,
-        user.Account.access_token
-      )
+        user.Account.access_token,
+        user.Account.refresh_token
+      ).then((totalFollowerCount) => ({ user, totalFollowerCount }))
+    })
+    const results = await Promise.all(promises)
 
-      if (totalFollowerCount !== null) {
-        await prisma.user.update({
-          where: { id: user.id },
-          data: { followers: totalFollowerCount },
-        })
-        console.log(
-          `Updated followers for user ${user.name} to ${totalFollowerCount}`
+    // Filter out null results and update the users
+    for (const result of results) {
+      if (result && result.totalFollowerCount !== null) {
+        updates.push(
+          prisma.user.update({
+            where: { id: result.user.id },
+            data: { followers: result.totalFollowerCount },
+          })
         )
-      } else {
-        // console.log(`Failed to update followers for user ${user.name}`)
-        // do nothing
+
+        console.log(
+          `Updated followers for user ${result.user.name} to ${result.totalFollowerCount}`
+        )
+      } else if (result) {
+        updates.push(
+          prisma.account.update({
+            where: {
+              providerAccountId: result.user.Account.providerAccountId,
+            },
+            data: { requires_refresh: true },
+          })
+        )
+
+        console.error(`Failed to update followers for user ${result.user.name}`)
       }
     }
+
+    // Execute all updates in a single transaction
+    await prisma.$transaction(updates)
+
+    page++
   } catch (error) {
-    console.error('Failed to update followers:', error)
+    console.error('Big catch on something', error)
   }
+}
+
+async function checkInvalidTokens() {
+  // get the access token from usersWithErrors providerAccountIds
+  for (const providerAccountId of usersWithErrors) {
+    const user = await prisma.user.findFirst({
+      where: {
+        Account: {
+          providerAccountId,
+        },
+      },
+      include: {
+        Account: true,
+      },
+    })
+
+    if (!user) {
+      console.error(`User not found for providerAccountId ${providerAccountId}`)
+      continue
+    }
+
+    const accessToken = user.Account?.access_token
+    if (!accessToken) {
+      console.error(
+        `Access token not found for providerAccountId ${providerAccountId}`
+      )
+      continue
+    }
+
+    const data = await validateAccessToken(accessToken)
+    if (!data) {
+      console.error(
+        `Failed to validate access token for providerAccountId ${providerAccountId}`
+      )
+      // try to refresh the token
+      const newAccessToken = await refreshAccessToken(
+        user.Account.refresh_token
+      )
+      if (newAccessToken) {
+        console.log(
+          `Refreshed access token for providerAccountId ${providerAccountId}`
+        )
+      } else {
+        console.error(
+          `Failed to refresh access token for providerAccountId ${providerAccountId}`
+        )
+      }
+      continue
+    }
+
+    if (data.client_id !== process.env.TWITCH_CLIENT_ID) {
+      console.error(
+        `Invalid client ID for providerAccountId ${providerAccountId}`
+      )
+      continue
+    }
+
+    if (data.login !== user.name) {
+      console.error(`Invalid user for providerAccountId ${providerAccountId}`)
+      continue
+    }
+
+    if (data.expires_in < 60) {
+      console.error(
+        `Access token for providerAccountId ${providerAccountId} is about to expire`
+      )
+    }
+
+    console.log(
+      `Access token for providerAccountId ${providerAccountId} is valid`
+    )
+  }
+}
+
+// with requires_refresh = true
+async function updateInvalidTokens() {
+  const updates = []
+  for (const providerAccountId of usersWithErrors) {
+    updates.push(
+      prisma.account.update({
+        where: { providerAccountId: providerAccountId },
+        data: { requires_refresh: true },
+      })
+    )
+  }
+
+  await prisma.$transaction(updates)
 }
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -80,61 +252,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(500).end()
   }
 
-  try {
-    const topLive = await prisma.user.findMany({
-      take: 10,
-      where: {
-        followers: {
-          gte: 1000,
-        },
-        Bet: {
-          some: {
-            updatedAt: {
-              gte: new Date(new Date().getTime() - 60 * 60 * 1000), // 1 hour ago
-            },
-          },
-        },
-        stream_online: true,
-      },
-      orderBy: {
-        followers: 'desc',
-      },
-      select: {
-        name: true,
-        image: true,
-      },
-    })
-
-    const randomLive = await prisma.bet.findMany({
-      take: 10,
-      select: {
-        user: {
-          select: {
-            name: true,
-            image: true,
-          },
-        },
-      },
-      where: {
-        updatedAt: {
-          gte: new Date(new Date().getTime() - 60 * 60 * 1000), // 1 hour ago
-        },
-        user: {
-          name: {
-            notIn: topLive.map((bet) => bet.name),
-          },
-          stream_online: true,
-        },
-      },
-    })
-
-    return res.status(200).json({
-      topLive,
-      randomLive: randomLive.map((bet) => bet.user),
-    })
-  } catch (error) {
-    return res.status(500).end()
-  }
+  await updateFollows(res)
+  // await updateInvalidTokens()
+  res.end()
 }
 
 export default withMethods(['GET'], handler)
