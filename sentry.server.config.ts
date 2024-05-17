305976493cca932f455c05389770ced619f0e0fa
Matt <Geczy@users.noreply.github.com>
2024-05-17 18:10:33 -0500

    Revert "fix: use the old sentry project"
    This reverts commit df4449636c8418581efe8dde9db0294172ab1d63.

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,21 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs'
+import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+Sentry.init({
+  dsn: "https://1fd7672a9246f3de827f50c5498203e5@o4506527898468352.ingest.us.sentry.io/4507269398855680",
 
-if (SENTRY_DSN) {
-  Sentry.init({
-    dsn: SENTRY_DSN,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-
-    // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
-  })
-}
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
