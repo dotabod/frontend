df4449636c8418581efe8dde9db0294172ab1d63
Matt <Geczy@users.noreply.github.com>
2024-05-17 17:55:34 -0500

    fix: use the old sentry project

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,18 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
-Sentry.init({
-  dsn: "https://1fd7672a9246f3de827f50c5498203e5@o4506527898468352.ingest.us.sentry.io/4507269398855680",
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
+
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  })
+}
