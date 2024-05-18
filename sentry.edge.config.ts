f68aeddbcb3e0d8f807945cb185a5a8f79290ea0
Matt <Geczy@users.noreply.github.com>
2024-05-18 08:14:53 -0500

    Reapply "chore: update sentry config"
    This reverts commit ec31a5051101c13ecb3bbdc3d7101603c594bab8.

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1fd7672a9246f3de827f50c5498203e5@o4506527898468352.ingest.us.sentry.io/4507269398855680",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
