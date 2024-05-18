6faa1ee247f4d53bb4a6656655d5ff56a1599b36
Matt <Geczy@users.noreply.github.com>
2024-05-18 08:33:52 -0500

    fix sentry (#143)

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
 
 const { withSentryConfig } = require('@sentry/nextjs')
 
-const moduleExports = {
+const nextConfig = {
   // Your existing module.exports
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
@@ -25,31 +25,44 @@ const moduleExports = {
       'avatars.cloudflare.steamstatic.com',
     ],
   },
-
-  sentry: {
-    release: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-    autoInstrumentMiddleware: !!process.env.NEXT_PUBLIC_SENTRY_DSN,
-    disableServerWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
-    disableClientWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
-  },
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+module.exports = withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: 'mgates-llc',
+  project: 'dotabod-frontend',
 
   release: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
-  autoInstrumentMiddleware: !!process.env.NEXT_PUBLIC_SENTRY_DSN,
-  disableServerWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
-  disableClientWebpackPlugin: !process.env.NEXT_PUBLIC_SENTRY_DSN,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: '/monitoring',
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+})
