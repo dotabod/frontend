b59311bddd4de35c680f2df1b9b37b54caece66b
Matt <Geczy@users.noreply.github.com>
2024-06-16 20:52:09 -0500

    new: 7tv live emote events when added/removed
    closes https://github.com/dotabod/frontend/issues/148
    closes https://github.com/dotabod/frontend/issues/27

--- src/components/Dashboard/ChatBot.tsx
+++ src/components/Dashboard/ChatBot.tsx
@@ -8,75 +8,134 @@ import { useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import TwitchFetcher from 'twitch-fetcher'
 import MmrForm from './Features/MmrForm'
 
-const SevenTVBaseURL = (id: string) => `https://7tv.app/emotes/${id}`
-const SevenTVBaseEmoteURL = (id: string) =>
-  `https://cdn.7tv.app/emote/${id}/2x.webp`
-const BttvBaseURL = (id: string) => `https://betterttv.com/emotes/${id}`
-const BttvBaseEmoteURL = (id: string) =>
-  `https://cdn.betterttv.net/emote/${id}/2x.webp`
+const SevenTVBaseURL = (id) => `https://7tv.app/emotes/${id}`
+const SevenTVBaseEmoteURL = (id) => `https://cdn.7tv.app/emote/${id}/2x.webp`
+const BttvBaseURL = (id) => `https://betterttv.com/emotes/${id}`
+const BttvBaseEmoteURL = (id) => `https://cdn.betterttv.net/emote/${id}/2x.webp`
 
 const emotesRequired = [
-  {
-    label: 'HECANT',
-    id: '62978b4c441e9cea5e91f9e7',
-  },
-  {
-    label: 'Okayeg',
-    id: '603caa69faf3a00014dff0b1',
-  },
-  {
-    label: 'Happi',
-    bttv: true,
-    id: '634042bce6cf26500b42ce56',
-  },
+  { label: 'HECANT', id: '62978b4c441e9cea5e91f9e7' },
+  { label: 'Okayeg', id: '603caa69faf3a00014dff0b1' },
+  { label: 'Happi', bttv: true, id: '634042bce6cf26500b42ce56' },
   { label: 'Madge', id: '60a95f109d598ea72fad13bd' },
   { label: 'POGGIES', id: '60af1b5a35c50a77926314ad' },
-  {
-    label: 'PepeLaugh',
-    id: '60420e3f77137b000de9e675',
-  },
+  { label: 'PepeLaugh', id: '60420e3f77137b000de9e675' },
   { label: 'ICANT', id: '61e2d59077175547b4254999' },
   { label: 'BASED', id: '6043181d1d4963000d9dae39' },
   { label: 'Chatting', id: '60ef410f48cde2fcc3eb5caa' },
-  {
-    label: 'massivePIDAS',
-    id: '6257e7a3131d4588262a7505',
-  },
+  { label: 'massivePIDAS', id: '6257e7a3131d4588262a7505' },
   { label: 'Sadge', id: '61630205c1ff9a17cc396522' },
   { label: 'EZ', id: '63071b80942ffb69e13d700f' },
   { label: 'Clap', id: '60aed217c9cf495e5be86812' },
-  {
-    label: 'peepoGamble',
-    id: '60d83a6277324757d60ae099',
-  },
-  {
-    label: 'PauseChamp',
-    id: '60b012a8e5a579561100b67f',
-  },
+  { label: 'peepoGamble', id: '60d83a6277324757d60ae099' },
+  { label: 'PauseChamp', id: '60b012a8e5a579561100b67f' },
 ]
+
+const SOCKET_URL = 'wss://events.7tv.io/v3'
+
+const fetchSevenTVTwitchUser = async (broadcaster_id) => {
+  try {
+    const url = `https://7tv.io/v3/users/twitch/${broadcaster_id}`
+    const response = await fetch(url, { method: 'GET' })
+    return await response.json()
+  } catch (error) {
+    console.error(error)
+  }
+  return null
+}
+
+let ws = null
+
+const connectWebSocket = async (broadcaster_id, setEmotes) => {
+  if (ws !== null) {
+    return
+  }
+
+  ws = new WebSocket(SOCKET_URL)
+
+  ws.onopen = async () => {
+    const id = await fetchSevenTVTwitchUser(broadcaster_id)
+    const subscribe = {
+      op: 35,
+      d: {
+        type: 'emote_set.update',
+        condition: { object_id: id.emote_set.id },
+      },
+    }
+    ws.send(JSON.stringify(subscribe))
+    console.log('7TV WebSocket connected in channel')
+  }
+
+  ws.onmessage = (event) => handleMessages(event, setEmotes)
+}
+
+const handleMessages = (event, setEmotes) => {
+  const data = JSON.parse(event.data)
+  if (data.op !== 0) return
+  const editor = data.d.body.actor.username || data.d.body.actor.display_name
+  let text = null
+  if (data.d.body.pushed) {
+    const emote = data.d.body.pushed[0].value.name
+    text = `emote ${emote} added by ${editor}`
+  }
+  if (data.d.body.updated) {
+    const emote = data.d.body.updated[0].old_value.name
+    const alias = data.d.body.updated[0].value.name
+    text = `emote ${emote} renamed to ${alias} by ${editor}`
+  }
+  if (data.d.body.pulled) {
+    const emote = data.d.body.pulled[0].old_value.name
+    text = `emote ${emote} removed by ${editor}`
+  }
+
+  console.log(text)
+  // Update the emote state
+  if (data.d.body.pushed) {
+    setEmotes((prev) => [
+      ...prev,
+      ...data.d.body.pushed.map((item) => item.value),
+    ])
+  }
+
+  if (data.d.body.pulled) {
+    setEmotes((prev) =>
+      prev.filter(
+        (emote) =>
+          !data.d.body.pulled.find((r) => r.old_value.name === emote.name)
+      )
+    )
+  }
+
+  if (data.d.body.updated) {
+    setEmotes((prev) =>
+      prev.map(
+        (emote) =>
+          data.d.body.updated.find((u) => u.value.name === emote.name) || emote
+      )
+    )
+  }
+}
 
 export default function ChatBot() {
   const session = useSession()
   const [emotes, setEmotes] = useState([])
   const clipboard = useClipboard({ timeout: 2000 })
 
+  console.log(emotes)
+
   useEffect(() => {
     if (!session.data.user.twitchId) return
-
-    const emoteFetcher = new TwitchFetcher()
-    emoteFetcher
-      .getEmotesByID(session.data.user.twitchId, {
-        ffz: true,
-        '7tv': true,
-        bttv: true,
-      })
-      .then(setEmotes)
-      .catch((e) => {
-        //
-      })
+    ;(async () => {
+      const user = await fetchSevenTVTwitchUser(session.data.user.twitchId)
+      if (Array.isArray(user?.emote_set?.emotes)) {
+        setEmotes(user.emote_set.emotes)
+      }
+      if (user?.emote_set?.id) {
+        connectWebSocket(session.data.user.twitchId, setEmotes)
+      }
+    })()
   }, [session.data.user.twitchId])
 
   return (
@@ -106,12 +165,12 @@ export default function ChatBot() {
             }}
             dataSource={emotesRequired.sort((a, b) => {
               // if it's found in emotes, put it at the bottom
-              if (emotes.find((e) => e.code === a.label)) return 1
-              if (emotes.find((e) => e.code === b.label)) return -1
+              if (emotes.find((e) => e.name === a.label)) return 1
+              if (emotes.find((e) => e.name === b.label)) return -1
               return 0
             })}
             renderItem={({ bttv, id, label }) => {
-              const added = emotes.find((e) => e.code === label)
+              const added = emotes.find((e) => e.name === label)
 
               return (
                 <List.Item key={label}>
