0f7b764e260117039edff1b1847729a02b3ad68a
Matt <Geczy@users.noreply.github.com>
2024-07-13 09:45:57 -0500

    fix: final step updates

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,27 +3,33 @@ import DashboardShell from '@/components/Dashboard/DashboardShell'
 import ExportCFG from '@/components/Dashboard/ExportCFG'
 import Header from '@/components/Dashboard/Header'
 import OBSOverlay from '@/components/Dashboard/OBSOverlay'
+import { fetcher } from '@/lib/fetcher'
 import { Card } from '@/ui/card'
-import { Button, Steps } from 'antd'
+import { Alert, Button, Steps, Tag } from 'antd'
+import confetti from 'canvas-confetti'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { type ReactElement, useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 const SetupPage = () => {
+  const { data } = useSWR('/api/settings', fetcher)
+  const isLive = data?.stream_online
+
   const [active, setActive] = useState(0)
   const nextStep = () =>
     setActive((current) => (current < 3 ? current + 1 : current))
   const prevStep = () =>
     setActive((current) => (current > 0 ? current - 1 : current))
+  const maxStepIndex = 3
 
   const router = useRouter()
   const { step } = router.query
 
   useEffect(() => {
     // Assuming the maximum step index is 3 (for a total of 4 steps)
-    const maxStepIndex = 3
     const parsedStep = Number.parseInt(step as string)
 
     setActive(
@@ -32,6 +38,38 @@ const SetupPage = () => {
         : 0
     )
   }, [step]) // Dependency array, re-run effect when `step` changes
+
+  useEffect(() => {
+    if (active === maxStepIndex) {
+      const end = Date.now() + 1 * 1000
+      const colors = ['#a786ff', '#fd8bbc', '#eca184', '#f8deb1']
+
+      const frame = () => {
+        if (Date.now() > end) return
+
+        confetti({
+          particleCount: 2,
+          angle: 60,
+          spread: 55,
+          startVelocity: 60,
+          origin: { x: 0, y: 0.5 },
+          colors: colors,
+        })
+        confetti({
+          particleCount: 2,
+          angle: 120,
+          spread: 55,
+          startVelocity: 60,
+          origin: { x: 1, y: 0.5 },
+          colors: colors,
+        })
+
+        requestAnimationFrame(frame)
+      }
+
+      frame()
+    }
+  }, [active])
 
   const steps = [
     {
@@ -50,6 +88,16 @@ const SetupPage = () => {
       title: 'All done!',
       content: (
         <Card>
+          {!isLive && (
+            <div className="flex flex-row items-center justify-center">
+              <Alert
+                message="Your stream is offline, and Dotabod will only work once you start streaming and go online."
+                type="warning"
+                showIcon
+                className="max-w-2xl"
+              />
+            </div>
+          )}
           <div className="mb-4 space-x-2">
             <span>
               <b>That&apos;s it!</b> You&apos;re all set up.
@@ -63,25 +111,24 @@ const SetupPage = () => {
               height={28}
             />
           </div>
-          <div className="flex flex-row space-x-4">
-            <div className="flex-none">
-              <Image
-                alt="dotabod browser source properties"
-                width={284}
-                unoptimized
-                height={863}
-                className="rounded-xl"
-                src="/images/setup/play-vs-bots.png"
-              />
-            </div>
+          <div>
+            <p>
+              Test it by watching a live match. Type <Tag>!np</Tag> while
+              spectating and you should see Dotabod respond in your Twitch chat.
+              Visit the <Link href="overlay">Live Preview page</Link> to confirm
+              the overlay is showing. You should see the minimap blocker overlay
+              once you&apos;re in a match.
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center space-x-4">
             <div>
-              <p>
-                Test it by joining a bot match. Visit the{' '}
-                <Link href="overlay">Live Preview page</Link> to confirm the
-                overlay is showing. You should see the minimap blocker overlay
-                once you&apos;re in a match.
-              </p>
-              <p>Note: Dotabod will only work if your stream is online.</p>
+              <Image
+                alt="watch live match"
+                width={1320}
+                height={3161}
+                className="rounded-xl"
+                src="https://i.imgur.com/dwa1ie0.png"
+              />
             </div>
           </div>
         </Card>
