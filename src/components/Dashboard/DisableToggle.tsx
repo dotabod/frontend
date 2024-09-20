33d8ea523ece288e869a8d9c68fc43d31d10126f
Matt Gates <Geczy@users.noreply.github.com>
2024-09-20 10:21:01 -0500

    ui: add toggle to notification

--- src/components/Dashboard/DisableToggle.tsx
+++ src/components/Dashboard/DisableToggle.tsx
@@ -6,11 +6,8 @@ import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 import useSWR from 'swr'
 
-export function DisableToggle() {
+const Toggle = () => {
   const { data } = useSWR('/api/check-ban', fetcher)
-  const { notification } = App.useApp()
-  const user = useSession()?.data?.user
-
   const {
     data: isDotabodDisabled,
     loading,
@@ -18,6 +15,59 @@ export function DisableToggle() {
   } = useUpdateSetting(Settings.commandDisable)
 
   const checkBanOrDisable = isDotabodDisabled || data?.banned
+
+  return (
+    <Tooltip
+      title={
+        !data?.banned && checkBanOrDisable
+          ? 'Click to enable Dotabod and start responding to game events and commands again.'
+          : 'Click to disable Dotabod and stop responding to game events and commands'
+      }
+    >
+      <label
+        htmlFor="disable-toggle"
+        className="cursor-pointer space-x-2 rounded flex flex-row items-center text-gray-300"
+      >
+        <Switch
+          id="disable-toggle"
+          loading={loading}
+          className="flex"
+          disabled={data?.banned}
+          checked={!checkBanOrDisable}
+          onChange={(checked) => updateSetting(!checked)}
+        />
+        <span className="text-clip text-nowrap">
+          Dotabod is {checkBanOrDisable ? 'disabled' : 'enabled'}
+        </span>
+      </label>
+    </Tooltip>
+  )
+}
+
+export function DisableToggle() {
+  const { data } = useSWR('/api/check-ban', fetcher)
+  const { notification } = App.useApp()
+  const user = useSession()?.data?.user
+  const { data: settingsData } = useSWR('/api/settings', fetcher)
+  const isLive = settingsData?.stream_online
+
+  const { data: isDotabodDisabled } = useUpdateSetting(Settings.commandDisable)
+
+  useEffect(() => {
+    if (isLive) {
+      notification.destroy('stream-offline')
+    } else {
+      notification.open({
+        key: 'stream-offline',
+        type: 'warning',
+        duration: 0,
+        placement: 'bottomLeft',
+        message: 'Your stream is offline.',
+        description:
+          'Dotabod will only work once you start streaming and go online.',
+      })
+    }
+  }, [isLive, notification])
 
   useEffect(() => {
     if (isDotabodDisabled) {
@@ -27,8 +77,15 @@ export function DisableToggle() {
         duration: 0,
         placement: 'bottomLeft',
         message: 'Dotabod is currently disabled.',
-        description:
-          'Click the toggle to enable it. You will not receive any game events or commands until you do.',
+        description: (
+          <div className="space-y-2">
+            <span>
+              Click the toggle to enable it. You will not receive any game
+              events or commands until you do.
+            </span>
+            <Toggle />
+          </div>
+        ),
       })
     } else {
       notification.destroy('dotabod-disabled')
@@ -67,30 +124,5 @@ export function DisableToggle() {
     }
   }, [data?.banned, notification, user?.name])
 
-  return (
-    <Tooltip
-      title={
-        !data?.banned && checkBanOrDisable
-          ? 'Click to enable Dotabod and start responding to game events and commands again.'
-          : 'Click to disable Dotabod and stop responding to game events and commands'
-      }
-    >
-      <label
-        htmlFor="disable-toggle"
-        className="cursor-pointer space-x-2 rounded flex flex-row items-center text-gray-300"
-      >
-        <Switch
-          id="disable-toggle"
-          loading={loading}
-          className="flex"
-          disabled={data?.banned}
-          checked={!checkBanOrDisable}
-          onChange={(checked) => updateSetting(!checked)}
-        />
-        <span className="text-clip text-nowrap">
-          Dotabod is {checkBanOrDisable ? 'disabled' : 'enabled'}
-        </span>
-      </label>
-    </Tooltip>
-  )
+  return <Toggle />
 }
