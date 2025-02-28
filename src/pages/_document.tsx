ac77e15b8f8545c2dda8f44fde2203720693a546
Matt Gates <Geczy@users.noreply.github.com>
2025-02-28 08:39:27 -0600

    feat: Add Ant Design CSS-in-JS style extraction to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,35 +1,48 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs'
+import type { DocumentContext } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html className="h-full bg-transparent" lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#000000" />
-      </Head>
-      <body className="min-h-screen bg-transparent">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+const MyDocument = () => (
+  <Html className='h-full bg-transparent' lang='en'>
+    <Head>
+      <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+      <meta name='msapplication-TileColor' content='#da532c' />
+      <meta name='theme-color' content='#000000' />
+    </Head>
+    <body className='min-h-screen bg-transparent'>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const cache = createCache()
+  const originalRenderPage = ctx.renderPage
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => (
+        <StyleProvider cache={cache}>
+          <App {...props} />
+        </StyleProvider>
+      ),
+    })
+
+  const initialProps = await Document.getInitialProps(ctx)
+  const style = extractStyle(cache, true)
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        <style dangerouslySetInnerHTML={{ __html: style }} />
+      </>
+    ),
+  }
 }
+
+export default MyDocument
