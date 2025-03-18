70608274906baac05d2df932b34ff0ed635ed2c8
Matt Gates <Geczy@users.noreply.github.com>
2025-03-18 17:45:22 -0500

    Refactor TwitchChat Component for Improved Structure and Readability
    - Changed the layout of the command and response sections to use <span> elements instead of <div>, enhancing inline display and reducing unnecessary block elements.
    - Updated class names for better consistency and readability in the chat display.
    - Adjusted the mapping of responses to include unique keys for each response, improving React's rendering efficiency.

--- src/components/TwitchChat.tsx
+++ src/components/TwitchChat.tsx
@@ -20,23 +20,23 @@ export default function TwitchChat({
       className={clsx(className, 'mt-2 max-w-xs rounded-sm border p-2 text-sm', 'border-gray-700')}
     >
       {command && (
-        <div>
+        <span>
           {modOnly && <ModImage />}
           <span className='font-bold text-[#8a2be2]'>techleed</span>
-          <span className='mr-1'>:</span>
-          <div className='inline'>{command}</div>
-        </div>
+          <span className='mr-1'>: </span>
+          <span>{command}</span>
+        </span>
       )}
-      <div className='space-y-1'>
-        {responses.map((response, i) => (
-          <div key={i}>
+      <span>
+        {responses.map((response, responseIndex) => (
+          <div key={`chat-response-${responseIndex}`}>
             <ModImage />
             <span className='font-bold text-[#c90909]'>dotabod</span>
-            <span className='mr-1'>:</span>
-            <div className={clsx('inline', 'text-[#EFEFF1]')}>{response}</div>
+            <span className=''>: </span>
+            <span className='text-[#EFEFF1]'>{response}</span>
           </div>
         ))}
-      </div>
+      </span>
     </div>
   )
 }
