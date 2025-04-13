1bcceeb07a672f19addb03268c28a0fcc77008b2
Matt <Geczy@users.noreply.github.com>
2025-04-13 15:30:54 -0500

    feat(Hero): enhance homepage layout and improve streamer engagement messaging

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -3,6 +3,7 @@ import { PhoneFrame } from '@/components/Homepage/PhoneFrame'
 import { fetcher } from '@/lib/fetcher'
 import { useTrack } from '@/lib/track'
 import { Button, Popover, Skeleton } from 'antd'
+import { LucideHome, LucideTerminalSquare } from 'lucide-react'
 import { signIn, useSession } from 'next-auth/react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -12,7 +13,6 @@ import dotaLogo from 'src/images/logos/dota.svg'
 import TwitchSvg from 'src/images/logos/twitch.svg'
 import useSWR from 'swr'
 import { LiveIcon } from './LiveIcon'
-import { LucideHome, LucideTerminalSquare } from 'lucide-react'
 
 const TwitchUser = ({
   image,
@@ -129,23 +129,29 @@ export function Hero() {
   return (
     <div className='overflow-hidden py-4 sm:py-4 lg:pb-4 xl:pb-4'>
       <Container>
-        <div className='lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20'>
+        <div className='lg:grid lg:grid-cols-12 lg:gap-x-8 lg:gap-y-20 items-center'>
           <div className='relative z-10 mx-auto max-w-2xl lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6'>
-            <h1 className='flex items-center space-x-2 text-4xl font-medium tracking-tight text-gray-200'>
-              <span>Welcome, {name}</span>
-              <Image
-                src='/images/emotes/peepoclap.webp'
-                unoptimized
-                width={38}
-                height={38}
-                alt='peepoclap'
-              />
-            </h1>
-            <p className='mt-6 text-lg text-gray-300'>
-              Dotabod is the all-in-one toolkit that transforms your Dota 2 stream, providing
-              real-time stats, automated Twitch predictions, and intelligent overlays that engage
-              your audience effortlessly.
-            </p>
+            <div className='space-y-4'>
+              <h1 className='text-4xl font-bold tracking-tight text-gray-200 sm:text-5xl'>
+                The greatest tool for Dota 2 streamers
+              </h1>
+              <h2 className='flex items-center gap-2 text-2xl font-medium tracking-tight text-gray-200'>
+                Smart Predictions • MMR Tracking • Interactive Chat
+                <Image
+                  src='/images/emotes/peepoclap.webp'
+                  unoptimized
+                  width={38}
+                  height={38}
+                  alt='peepoclap'
+                  className='inline-block'
+                />
+              </h2>
+              <p className='text-lg text-gray-300'>
+                Join thousands of streamers using Dotabod to enhance their streams with automated
+                predictions, real-time stats, and dynamic overlays that keep viewers engaged and
+                coming back for more.
+              </p>
+            </div>
             <div className='mt-8 flex flex-wrap gap-x-6 gap-y-4'>
               <Link href='/dashboard'>
                 <Button type='primary'>
@@ -182,64 +188,72 @@ export function Hero() {
                 />
               </PhoneFrame>
             </div>
-          </div>
-        </div>
 
-        <div className='relative lg:col-span-7 xl:col-span-6'>
-          <div className='flex items-center space-x-2 text-center text-sm font-semibold text-gray-300 lg:text-left'>
-            <Image src={TwitchSvg} width={18} height={18} alt='twitch logo' />
-            <span>
-              Trusted by Over {new Intl.NumberFormat().format(20000)} Twitch Streamers Including:
-            </span>
-            <LiveIcon />
-          </div>
-        </div>
+            <div key='twitch-streamer-list'>
+              <div className='relative lg:col-span-7 xl:col-span-6'>
+                <div className='flex items-center space-x-2 text-center text-sm font-semibold text-gray-300 lg:text-left'>
+                  <Image src={TwitchSvg} width={18} height={18} alt='twitch logo' />
+                  <span>
+                    Trusted by Over {new Intl.NumberFormat().format(20000)} Twitch Streamers
+                    Including:
+                  </span>
+                  <LiveIcon />
+                </div>
+              </div>
 
-        <div className='relative lg:col-span-7 xl:col-span-6'>
-          {isLoading || !users?.topLive?.length ? (
-            <ul className='mx-auto flex max-w-xl flex-wrap justify-center lg:mx-0 lg:justify-start pt-4'>
-              {[...Array(10)].map((_, i) => (
-                <li key={`skeleton-${i}`} className='relative'>
-                  <div className='flex flex-col items-center space-y-2 px-4 py-2'>
-                    <Skeleton.Avatar active size={50} shape='square' style={{ borderRadius: 8 }} />
-                    <Skeleton.Input
-                      block={false}
-                      active
-                      size='small'
-                      style={{ height: 5, minWidth: 55, width: 55 }}
-                    />
-                  </div>
-                </li>
-              ))}
-            </ul>
-          ) : users?.topLive?.length && users?.topLive?.length > 0 ? (
-            <ul className='mx-auto flex max-w-xl flex-wrap justify-center lg:mx-0 lg:justify-start'>
-              {users?.topLive?.map(({ name, image }) => (
-                <TwitchUser
-                  key={name}
-                  last={false}
-                  name={name}
-                  image={image}
-                  onClick={() => {
-                    track('homepage - top live twitch profile')
-                  }}
-                />
-              ))}
-              {!userInTopLive && (
-                <TwitchUser
-                  key='You?'
-                  last={true}
-                  name='You?'
-                  onClick={() => {
-                    track('homepage - static twitch profile')
-                  }}
-                  image='/images/hero/default.png'
-                />
-              )}
-            </ul>
-          ) : (
-            <p className='text-center text-gray-300'>No top streamers found</p>
-          )}
+              <div className='relative lg:col-span-7 xl:col-span-6'>
+                {isLoading || !users?.topLive?.length ? (
+                  <ul className='mx-auto flex max-w-xl flex-wrap justify-center lg:mx-0 lg:justify-start pt-4'>
+                    {[...Array(10)].map((_, i) => (
+                      <li key={`skeleton-${i}`} className='relative'>
+                        <div className='flex flex-col items-center space-y-2 px-4 py-2'>
+                          <Skeleton.Avatar
+                            active
+                            size={50}
+                            shape='square'
+                            style={{ borderRadius: 8 }}
+                          />
+                          <Skeleton.Input
+                            block={false}
+                            active
+                            size='small'
+                            style={{ height: 5, minWidth: 55, width: 55 }}
+                          />
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                ) : users?.topLive?.length && users?.topLive?.length > 0 ? (
+                  <ul className='mx-auto flex max-w-xl flex-wrap justify-center lg:mx-0 lg:justify-start'>
+                    {users?.topLive?.map(({ name, image }) => (
+                      <TwitchUser
+                        key={name}
+                        last={false}
+                        name={name}
+                        image={image}
+                        onClick={() => {
+                          track('homepage - top live twitch profile')
+                        }}
+                      />
+                    ))}
+                    {!userInTopLive && (
+                      <TwitchUser
+                        key='You?'
+                        last={true}
+                        name='You?'
+                        onClick={() => {
+                          track('homepage - static twitch profile')
+                        }}
+                        image='/images/hero/default.png'
+                      />
+                    )}
+                  </ul>
+                ) : (
+                  <p className='text-center text-gray-300'>No top streamers found</p>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
